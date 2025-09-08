5847ce2792df26134b223f04158005ba865da3df
Vinay Kumar Chopra <147031659+HUN-sp@users.noreply.github.com>
2025-09-08 18:07:31 +0530

    Completed Beverages-decorator

--- beverages_decorator/Client.java
+++ beverages_decorator/Client.java
@@ -1,14 +1,20 @@
 package beverages_decorator;
 
-public class Client {
+	public class Client {
 
 	public static void main(String[] args) {
+
 		Beverage coffee = new Cappuccino();
-		
-        System.out.println("Coffee");
-		System.out.println(coffee.cost());
+		System.out.println("Plain Cappuccino: " + coffee.cost());
 		
 		
+		
+		Beverage chocolateCoffee = new Chocolate(new Cappuccino());
+		System.out.println("Cappuccino with Chocolate: " + chocolateCoffee.cost());
+		
+		Beverage espressoSpecial = new Whip(new Cappuccino());
+		System.out.println("Espresso with Milk and Whip: " + espressoSpecial.cost());
+
 
 	}
 
